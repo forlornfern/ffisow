a0688318b54472a870fdeb4af2cda5824ecd8cb6
ForlornFern <dinozawr935@gmail.com>
2026-05-09 19:58:01 +0300

    feat: implement ProgressReader

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
+	"io"
 	"os"
 
 	"charm.land/log/v2"
+	"github.com/forlornfern/ffisow/internal"
 	"github.com/spf13/cobra"
 )
 
@@ -21,8 +23,33 @@ var (
 			}
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
+			src, err := os.Open(args[0])
+			if err != nil {
+				return err
+			}
+			dst, err := os.OpenFile(args[1], os.O_WRONLY|os.O_SYNC, 0)
+			if err != nil {
+				return err
+			}
+			defer src.Close()
+			defer dst.Close()
+			info, err := src.Stat()
+			if err != nil {
+				return err
+			}
+			size := info.Size()
 
-			return nil
+			pr := &internal.ProgressReader{
+				Reader: src,
+				Total:  size,
+			}
+
+			buf := make([]byte, 5*1024*1024)
+			_, err = io.CopyBuffer(dst, pr, buf)
+			if err != nil {
+				return err
+			}
+			return dst.Sync()
 		},
 	}
 )
